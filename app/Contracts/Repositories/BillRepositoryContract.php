c0abe89507611799018b9cc476c941aa3e664233
Jacob Boomgaarden <jcboomgaarden@gmail.com>
2017-08-20 15:40:56 +0300

    added new directory and contracts for bills/billiterations

---
+++ app/Contracts/Repositories/BillRepositoryContract.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Quixotical
+ * Date: 8/20/17
+ * Time: 16:24
+ */
+declare(strict_types=1);
+namespace App\Contracts\Repositories;
+
+interface BillRepositoryContract
+{
+
+}
