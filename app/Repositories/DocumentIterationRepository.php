8c916f1b4ab0ef6789a9821d27807f179ae4767a
Jacob Boomgaarden <jcboomgaarden@gmail.com>
2017-08-21 12:30:47 +0300

    added create function & createForeign helper function

--- app/Repositories/DocumentIterationRepository.php
+++ app/Repositories/DocumentIterationRepository.php
@@ -8,7 +8,9 @@
 namespace App\Repositories;
 
 use App\Contracts\Repositories\DocumentIterationRepositoryContract;
+use App\Models\Document;
 use App\Models\DocumentIteration;
+use App\Models\EloquentAbstractModel;
 
 /**
  * Class DocumentIterationRepository
@@ -19,9 +21,37 @@ class DocumentIterationRepository implements DocumentIterationRepositoryContract
     /**
      * @var DocumentIteration $DocumentIteration - the document model for this repository instance
      */
-    protected $DocumentIteration;
+    protected $documentIteration;
 
+    /**
+     * @var string ending of foreign key fields
+     */
+    protected $id = '_id';
+
+    /**
+     * Constructor for the DocumentIterationRepository
+     * @param DocumentIteration $DocumentIteration
+     */
     public function __construct(DocumentIteration $DocumentIteration){
-        $this->DocumentIteration = $DocumentIteration;
+        $this->documentIteration = $DocumentIteration;
+    }
+
+    /**
+     * Create new document Iteration
+     * @param array $data
+     * @param Document $document
+     * @return $this|\Illuminate\Database\Eloquent\Model
+     * @internal param EloquentAbstractModel $parent
+     */
+    public function create(array $data = [], Document $document)
+    {
+        $tableName = $document->getTable();
+        $foreignKey = $this->createForeign($tableName);
+        $dataAndFk = array_add($data, $foreignKey, $document->id);
+        return $this->documentIteration->create($dataAndFk);
+    }
+
+    private function createForeign($tableName){
+        return str_singular($tableName) . $this->id;
     }
 }
